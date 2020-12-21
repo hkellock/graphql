29159ffe58b3454f6d190e0032874efb57d33aba
Henri Kellock <henri.kellock@wunderdog.fi>
2020-12-21 18:32:20 +0200

    WEB: Add prettier

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,16 +4,13 @@ import { client } from './client';
 import './App.css';
 import Todo from './components/Todo/Todo';
 
-const App: React.FC = () =>
-  (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          Todo app
-        </header>
-        <Todo />
-      </div>
-    </ApolloProvider>
-  );
+const App: React.FC = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <header className="App-header">Todo app</header>
+      <Todo />
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
--- web/src/components/Todo/Todo.tsx
+++ web/src/components/Todo/Todo.tsx
@@ -2,42 +2,38 @@ import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
 type TodoItem = {
-    id: string;
-    title: string;
-    description?: string;
-    completed: boolean;
-}
+  id: string;
+  title: string;
+  description?: string;
+  completed: boolean;
+};
 
 type Todos = {
-    todos: TodoItem[];
-}
+  todos: TodoItem[];
+};
 
 const TODO_QUERY = gql`
-    query {
-        todos {
-            id
-            title
-        }
+  query {
+    todos {
+      id
+      title
     }
+  }
 `;
 
-const Todo: React.FC = () =>
-    {
-        const { loading, error, data } = useQuery<Todos>(TODO_QUERY);
+const Todo: React.FC = () => {
+  const { loading, error, data } = useQuery<Todos>(TODO_QUERY);
 
-        if (loading) return <p>Loading...</p>;
-        if (error || !data) return <p>Error!</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error || !data) return <p>Error!</p>;
 
-        return (
-            <ul>
-                {
-                    data.todos.map((todo) => (
-                        <li key={todo.id}>
-                            title: {todo.title}
-                        </li>
-                    ))
-                }
-            </ul>
-        );}
+  return (
+    <ul>
+      {data.todos.map((todo) => (
+        <li key={todo.id}>title: {todo.title}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default Todo;
