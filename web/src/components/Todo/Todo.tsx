dda76effc727672c9692a36195393dfe44ceb852
Henri Kellock <henri.kellock@wunderdog.fi>
2020-12-21 18:11:53 +0200

    WEB: Add types for query

--- web/src/components/Todo/Todo.tsx
+++ web/src/components/Todo/Todo.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
+type TodoItem = {
+    id: string;
+    title: string;
+    description?: string;
+    completed: boolean;
+}
+
+type Todos = {
+    todos: TodoItem[];
+}
+
 const TODO_QUERY = gql`
     query {
         todos {
             id
+            title
         }
     }
 `;
 
 const Todo: React.FC = () =>
     {
-        const { loading, error, data } = useQuery(TODO_QUERY);
-        
+        const { loading, error, data } = useQuery<Todos>(TODO_QUERY);
+
         if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error!</p>;
+        if (error || !data) return <p>Error!</p>;
 
         return (
             <ul>
                 {
-                    data.todos.map(({ id }: { id: string }) => (
-                        <li key={id}>
-                            id: {id}
+                    data.todos.map((todo) => (
+                        <li key={todo.id}>
+                            title: {todo.title}
                         </li>
                     ))
                 }
